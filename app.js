0c22f8d3b8bfc5c2e31e5361784f47593d666b93
dandfres <168474016+dandfres@users.noreply.github.com>
2025-06-15 13:33:42 -0500

    refactor: se optimizo la seccion settings feat: se agrego la funcionalidad para pintar las celdas mouseover

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-// Toma y muestra el valor del input (range)
-const selectedRange = document.querySelector('#selected-range');
-const rangePreview = document.querySelector('.range-preview');
+// const container = document.querySelector('.container');
+const gridContainer = document.querySelector('#grid-container');
+const gridSize = document.querySelector('.grid-size');
 
-selectedRange.addEventListener('input', () => {
-    rangePreview.textContent = `${selectedRange.value} x ${selectedRange.value}`;
-});
+// Obtiene y muestra el valor del input (range)
+const showSize = document.querySelector('.show-size');
+const selectedSize = document.querySelector('#selected-size');
+
+// Mostar el valor por defecto del grid
+gridRender(selectedSize.value);
+
+function handleEvent(e) {
+    switch (e.type) {
+        case 'input':
+            showSize.textContent = `${e.target.value} x ${e.target.value}`;
+            // console.log(e.type);
+            break;
+        case 'change':
+            gridRender(e.target.value);
+            // console.log(e.type);
+            break;
+        case 'mouseover':
+            if (e.target.classList.contains('grid-cell')) {
+                // console.log("pasaste por un div de grid container");
+                e.target.classList.add('painted');
+            }
+    };
+};
+
+gridSize.addEventListener('input', handleEvent);
+gridSize.addEventListener('change', handleEvent);
+gridContainer.addEventListener('mouseover', handleEvent);
+
+// Actualiza el grid
+function gridRender(value) {
+    gridContainer.replaceChildren();
+    const cellSize = 480 / value;
+    
+    for (let i = 0; i < value * value; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        gridContainer.appendChild(cell);
+    };
+};
