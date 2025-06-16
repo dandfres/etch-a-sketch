0fddb0866c5d26e1337898d52eb5f54c603a2d46
dandfres <168474016+dandfres@users.noreply.github.com>
2025-06-15 20:05:53 -0500

    feat: Se habilitó el botón cleaning

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
 // const container = document.querySelector('.container');
 const gridContainer = document.querySelector('#grid-container');
-const gridSize = document.querySelector('.grid-size');
-
-// Obtiene y muestra el valor del input (range)
+const size = document.querySelector('.size');
+const settings = document.querySelector('.settings')
 const showSize = document.querySelector('.show-size');
 const selectedSize = document.querySelector('#selected-size');
 
-// Mostar el valor por defecto del grid
-gridRender(selectedSize.value);
-
+// Manejador de eventos (funcion principal)
 function handleEvent(e) {
+    const element = e.target;
     switch (e.type) {
         case 'input':
-            showSize.textContent = `${e.target.value} x ${e.target.value}`;
-            // console.log(e.type);
+            showSize.textContent = `${element.value} x ${element.value}`;
+            // console.log(e.target);
             break;
         case 'change':
-            gridRender(e.target.value);
-            // console.log(e.type);
+            gridRender(element.value);
+            // console.log(e.target.id);
             break;
         case 'mouseover':
-            if (e.target.classList.contains('grid-cell')) {
-                // console.log("pasaste por un div de grid container");
-                e.target.classList.add('painted');
+            if (element.classList.contains('grid-cell')) {
+                element.style.backgroundColor = '#333';
+                // console.log(element);
+
             }
+            break;
+        case 'click':
+            if (e.target.classList.contains('btn')) {
+                if (e.target.classList.contains('cleaning')) {
+                    const nodes = document.querySelectorAll('.grid-cell');
+                    // console.log('presionaste el btn LIMPIAR')
+                    nodes.forEach(n => {
+                        n.style.backgroundColor = '';
+                    })
+                }
+            }
+            // gridContainer.replaceChildren();
+            break;
     };
 };
 
-gridSize.addEventListener('input', handleEvent);
-gridSize.addEventListener('change', handleEvent);
+// Mostar el grid por defecto
+gridRender(selectedSize.value);
+
+// Escucha el input range (preview)
+settings.addEventListener('input', handleEvent);
+// Escucha el input range (valor establecido)
+settings.addEventListener('change', handleEvent);
+// Escucha al boton cleaning
+settings.addEventListener('click', handleEvent)
+
+// Escucha el paso del mouse por encima de las celdas
 gridContainer.addEventListener('mouseover', handleEvent);
 
 // Actualiza el grid
@@ -38,9 +59,13 @@ function gridRender(value) {
     
     for (let i = 0; i < value * value; i++) {
         const cell = document.createElement('div');
+        cell.style.cssText = `
+        width: ${cellSize}px;
+        height: ${cellSize}px;
+        outline: 1px solid #ccc;
+        transition: 0.1s;
+        `
         cell.classList.add('grid-cell');
-        cell.style.width = `${cellSize}px`;
-        cell.style.height = `${cellSize}px`;
         gridContainer.appendChild(cell);
     };
 };
